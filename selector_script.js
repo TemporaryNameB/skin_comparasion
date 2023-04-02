5422efe4484f5ece832c17dc7982ec884418257b
GogaFoga <80777820+GogaFroga@users.noreply.github.com>
2023-04-02 15:14:29 +0300

    edited selector_script.js
    - removed code repeatability (that was even more cringe)

--- selector_script.js
+++ selector_script.js
@@ -1,53 +1,20 @@
-let select_flat = document.getElementById("image-select-flat");
-select_flat.addEventListener("mousedown", function(e) {
-  e.preventDefault();
-  let option = e.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
-
-let select_r301 = document.getElementById("image-select-r301");
-select_r301.addEventListener("mousedown", function(e) {
-  e.preventDefault();
-  let option = e.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
-
+const selectFlat = document.getElementById("image-select-flat");
+const selectR301 = document.getElementById("image-select-r301");
 let select_pk = document.getElementById("image-select-pk");
-select_pk.addEventListener("mousedown", function(e) {
-  e.preventDefault();
-  let option = e.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
-
 let select_wg = document.getElementById("image-select-wg");
-select_wg.addEventListener("mousedown", function(e) {
-  e.preventDefault();
-  let option = e.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
-
 let select_r99 = document.getElementById("image-select-r99");
-select_r99.addEventListener("mousedown", function(e) {
-  e.preventDefault();
-  let option = e.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
-
 let select_car = document.getElementById("image-select-car");
-select_car.addEventListener("mousedown", function(d) {
-  d.preventDefault();
-  let option = d.target;
-  if (option.tagName === "OPTION") {
-    option.selected = !option.selected;
-  }
-});
+
+function toggleOptionSelection(e) {
+    e.preventDefault();
+    if (e.target.tagName === "OPTION") {
+        e.target.selected = !e.target.selected;
+    }
+}
+
+selectFlat.addEventListener("mousedown", toggleOptionSelection);
+selectR301.addEventListener("mousedown", toggleOptionSelection);
+select_pk.addEventListener("mousedown", toggleOptionSelection);
+select_wg.addEventListener("mousedown", toggleOptionSelection);
+select_r99.addEventListener("mousedown", toggleOptionSelection);
+select_car.addEventListener("mousedown", toggleOptionSelection);
